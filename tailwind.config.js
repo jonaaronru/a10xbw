03e431cfe1d7680fecf445951686a956bf972886
imperatrona <xb2w1z@gmail.com>
2021-10-04 23:55:38 +0300

    css updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      'blackout': "#222222",
+      'gravelflint': "#bbbbbb",
+      white: colors.white,
+      black: colors.black,
+      gray: colors.gray,
+      blue: colors.blue
+    },
     extend: {},
   },
   variants: {
